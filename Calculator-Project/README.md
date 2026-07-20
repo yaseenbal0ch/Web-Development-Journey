4ea2c5a7104c7a8ada6f26a925e95fde125c8fb8
Muhammad Yaseen <yaseenonlinepk@gmail.com>
2026-07-20 16:12:23 +0000

    Calculator Project Completed

---
+++ Calculator-Project/README.md
@@ -0,0 +1,20 @@
+# 🧮 Calculator Project
+
+## Description
+A simple Calculator built using HTML, CSS, and JavaScript.
+
+## Features
+- Addition (+)
+- Subtraction (-)
+- Multiplication (*)
+- Division (/)
+- Percentage (%)
+- Clear Button (C)
+
+## Technologies Used
+- HTML5
+- CSS3
+- JavaScript
+
+## Author
+Muhammad Yaseen
---
+++ Calculator-Project/script.js
@@ -0,0 +1,40 @@
+const display = document.getElementById("display");
+const buttons = document.querySelectorAll("button");
+
+let expression = "";
+
+buttons.forEach(button => {
+
+    button.addEventListener("click", () => {
+
+        const value = button.innerText;
+
+        if (value === "C") {
+            expression = "";
+            display.value = "";
+        }
+
+        else if (value === "=") {
+
+            try {
+                expression = eval(expression).toString();
+                display.value = expression;
+            }
+
+            catch {
+                display.value = "Error";
+                expression = "";
+            }
+
+        }
+
+        else {
+
+            expression += value;
+            display.value = expression;
+
+        }
+
+    });
+
+});
